b97168f84e84ea9cff69bac7b56088867248ddfe
Kaka <hailongma61@gmail.com>
2024-04-09 20:35:27 +0800

    csv to json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, rust!");
+// rcli csv -i input.csv -o output.json --header -d ','
+use rcli::process_csv;
+use rcli::{Opts, SubCommand};
+
+use clap::Parser;
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+
+    Ok(())
 }
